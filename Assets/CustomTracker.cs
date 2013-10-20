883c2a14d3375f18cdeb1a55bab51d9c87a06f8d
przemekkubicki <kubik014@go2.pl>
2013-10-20 16:41:45 +0200

    Armadio v0.1
    Added:
    -new chair model
    -custom behaviour script
    -basic GUI control (rotating obj)
    -onscreen preloader & logotype

---
+++ Assets/CustomTracker.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomTracker : MonoBehaviour,ITrackableEventHandler  {
+
+
+    #region PRIVATE_MEMBER_VARIABLES
+  
+    private TrackableBehaviour mTrackableBehaviour;
+    private bool isRendered = false;
+    private GameObject Dragon;
+	private GUITexture controls;
+ 
+     
+    #endregion // PRIVATE_MEMBER_VARIABLES
+ 
+ 
+ 
+    #region UNTIY_MONOBEHAVIOUR_METHODS
+     
+    void Start()
+    {
+         
+        Dragon = GameObject.Find("Chair");
+        controls = (GameObject.Find("controls").GetComponent(typeof(GUITexture))) as GUITexture;
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+ 
+        OnTrackingLost();
+    }
+     
+    void Update()
+    {
+		int i = 0;
+		
+//		ARCamera = GameObject.Find("ARCamera");
+		
+		//Czy obiekt jest sledzony i renderowany
+        if( isRendered ){
+			//Przechwycenie wszystkich dotykow ekranu
+            while (i < Input.touchCount) {
+	            if (Input.GetTouch(i).phase == TouchPhase.Stationary) {
+	               if (controls.HitTest(Input.GetTouch(i).position)) {
+						Debug.Log("DOTKNIETY");
+						Dragon.transform.RotateAround(Vector3.down, 1 * Time.deltaTime);
+					}
+				}
+	            ++i;
+					
+        }
+			
+			
+			
+			//;
+             
+            //Debug.Log( Dragon.transform.position.ToString() );
+             
+        }
+    }
+ 
+    #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+ 
+ 
+ 
+    #region PUBLIC_METHODS
+ 
+    // Implementation of the ITrackableEventHandler function called when the
+    // tracking state changes.
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+     
+     
+ 
+    #endregion // PUBLIC_METHODS
+ 
+ 
+ 
+    #region PRIVATE_METHODS
+ 
+ 
+    private void OnTrackingFound()
+    {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>();
+ 
+        // Enable rendering:
+        foreach (Renderer component in rendererComponents) {
+            component.enabled = true;
+        }
+ 
+        Debug.Log("Trackable KRZESLO" + mTrackableBehaviour.TrackableName + " found");
+         
+        isRendered = true;
+
+    }
+ 
+ 
+    private void OnTrackingLost()
+    {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>();
+ 
+        // Disable rendering:
+        foreach (Renderer component in rendererComponents) {
+            component.enabled = false;
+        }
+ 
+        Debug.Log("Trackable KRZESLO" + mTrackableBehaviour.TrackableName + " lost");
+         
+        isRendered = false;
+    }
+	
+	#endregion 
+}
