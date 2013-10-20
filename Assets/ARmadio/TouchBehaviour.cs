249d348134c84343586295258b89e5a34c7f031d
przemekkubicki <kubik014@go2.pl>
2013-10-20 21:10:51 +0200

    Merge touch script

--- Assets/ARmadio/TouchBehaviour.cs
+++ Assets/ARmadio/TouchBehaviour.cs
@@ -7,6 +7,16 @@ public class TouchBehaviour : MonoBehaviour
 {
     private Vector3 lastPlanePoint;
     public bool touchToMove = false;
+	
+	
+	//zmienne odpowiedzialne za obracanie lub przesuwanie ekranu
+	public bool rotate = true;
+	public bool move = false;
+	
+	
+	//obiekty kontrolujace i kontrolowane
+	private GameObject ChairObj;
+	private GUITexture controls;
 
     void Awake()
     {
@@ -15,6 +25,9 @@ public class TouchBehaviour : MonoBehaviour
 
     void Start()
     {
+		//szukanie i przypisanie 
+		ChairObj = GameObject.Find("Chair");
+        controls = (GameObject.Find("controls").GetComponent(typeof(GUITexture))) as GUITexture;
     }
 
 
@@ -23,6 +36,7 @@ public class TouchBehaviour : MonoBehaviour
         if (touchToMove)
         {
             Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+
             foreach (var renderer in rendererComponents)
             {
                 /* TODO
@@ -33,37 +47,99 @@ public class TouchBehaviour : MonoBehaviour
                  * MoĹĽna teĹĽ rozwaĹĽyÄ‡ sposĂłb wykrycia dotyku
                  * W tej chwili moĹĽna przesuwaÄ‡ palcem obojÄ™tnie gdzie, a modele i tak bÄ™dÄ… lataĹ‚y po ekranie
                  * MoĹĽna by siÄ™ pokusiÄ‡ o wykrycie, czy na pewno trafiamy palcem w model (diagram voronoia?)
-                 * TODO */
+                 * TODO 
+                 * 
+                 * 
+                 * TODOV2
+                 * 
+                 * Odwoluje sie do znalezionego wczesniej ChairObjecta, nie do elementow z renderera
+                 * nie wiem czy ta petla jest potrzebna tam.
+                 * 
+                 * Domyslnie ustawione jest rotate na true, i move na false, ale trzeba to sprawdzic, bo mozna to
+                 * ustawiac z poziomu edytora Unity (jak beda dwa zaznaczone to raczej sie sypnie)
+                 * 
+                 * Label jest dodany na ekran, ale na chwile obecna nie dziala
+                 * 
+                 * Po kliknieciu w przycisk zmieniamy tryb obracania/przesuwania na drugi dostepny.
+                 * 
+                 * jesli dodamy zgrabne menu manipulujace tymi boolami to tak mogloby to wygladac (sterowanie, nie animacja :) )
+                 * 
+                 * 
+                 * 
+                 * 
+                 * */
+				
                 Plane targetPlane = new Plane(transform.up, transform.position);
                 // Dla kaĹĽdego wykrytego dotkniÄ™cia (multitouch wspierany)
                 foreach (Touch touch in Input.touches)
                 {
-                    //Gets the ray at position where the screen is touched
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    //Gets the position of ray along plane
-                    float dist = 0.0f;
-                    //Intersects ray with the plane. Sets dist to distance along the ray wher intersects
-                    targetPlane.Raycast(ray, out dist);
+					
+					//operacje dla rotacji (na razie w jednym kierunku)
+					if(rotate) {
+						if(touch.phase == TouchPhase.Began && controls.HitTest(touch.position)){
+								move = true;
+								rotate = false;
+								return;
+						}						
+						
+	                    //Gets the ray at position where the screen is touched
+	                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+	                    //Gets the position of ray along plane
+	                    float dist = 0.0f;
+	                    //Intersects ray with the plane. Sets dist to distance along the ray wher intersects
+	                    targetPlane.Raycast(ray, out dist);
+	
+	                    //Returns point dist along the ray.
+	                    Vector3 planePoint = ray.GetPoint(dist);
+	
+	                    //True if finger touch began.
+	                    if (touch.phase == TouchPhase.Began)
+	                    {
+	                        // Ustaw ostatni punkt na obecny
+	                        lastPlanePoint = planePoint;
+	                    }
+	                    else if (touch.phase == TouchPhase.Moved)
+	                    {
+	                        // Tu widac typowo jeden kierunek
+	                        ChairObj.transform.Rotate(Vector3.forward, 15 * Time.deltaTime);
+	                        lastPlanePoint = planePoint;
+	                    }
 
-                    //Returns point dist along the ray.
-                    Vector3 planePoint = ray.GetPoint(dist);
+						return;
+					}
+					
+					if(move) {
+						if(touch.phase == TouchPhase.Began && controls.HitTest(touch.position)){
+								move = false;
+								rotate = true;
+								return;
+						}						
+						
+	                    //Gets the ray at position where the screen is touched
+	                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+	                    //Gets the position of ray along plane
+	                    float dist = 0.0f;
+	                    //Intersects ray with the plane. Sets dist to distance along the ray wher intersects
+	                    targetPlane.Raycast(ray, out dist);
+	
+	                    //Returns point dist along the ray.
+	                    Vector3 planePoint = ray.GetPoint(dist);
+	
+	                    //True if finger touch began.
+	                    if (touch.phase == TouchPhase.Began)
+	                    {
+	                        // Ustaw ostatni punkt na obecny
+	                        lastPlanePoint = planePoint;
+	                    }
+	                    else if (touch.phase == TouchPhase.Moved)
+	                    {
+	                        // PrzesuĹ„ model i zmieĹ„ ostatni punkt
+	                        ChairObj.transform.position += planePoint - lastPlanePoint;
+	                        lastPlanePoint = planePoint;
+	                    }
 
-                    //True if finger touch began.
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        // Ustaw ostatni punkt na obecny
-                        lastPlanePoint = planePoint;
-                    }
-                    else if (touch.phase == TouchPhase.Moved)
-                    {
-                        // PrzesuĹ„ model i zmieĹ„ ostatni punkt
-                        renderer.transform.position += planePoint - lastPlanePoint;
-                        lastPlanePoint = planePoint;
-                    }
-                    else if (touch.phase == TouchPhase.Ended)
-                    {
-                        // 
-                    }
+						return;
+					}
                 }
             }
         }
