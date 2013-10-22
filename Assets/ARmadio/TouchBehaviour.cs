572ac72881339106601d24cde76d906699b4c27f
przemekkubicki <kubik014@go2.pl>
2013-10-22 21:55:54 +0200

    +Touch script: added scaling
    -Added scaling
    (rotation -> move -> scale -> rotation... ) etc. after joystick click

--- Assets/ARmadio/TouchBehaviour.cs
+++ Assets/ARmadio/TouchBehaviour.cs
@@ -12,10 +12,16 @@ public class TouchBehaviour : MonoBehaviour
 	//zmienne odpowiedzialne za obracanie lub przesuwanie ekranu
 	public bool rotate = true;
 	public bool move = false;
+	public bool scale = false;
 	
-	
+	//zmienne do rotacji
 	private float StartX=0;
 	private float StopX=0;
+	
+	//zmienne do skalowania
+	private Vector2 scaleStart1;
+	private Vector2 scaleStart2;
+	private float scaleStartDiff;
 	
 	
 	//obiekty kontrolujace i kontrolowane
@@ -62,11 +68,7 @@ public class TouchBehaviour : MonoBehaviour
                  * Domyslnie ustawione jest rotate na true, i move na false, ale trzeba to sprawdzic, bo mozna to
                  * ustawiac z poziomu edytora Unity (jak beda dwa zaznaczone to raczej sie sypnie)
                  * 
-                 * Label jest dodany na ekran, ale na chwile obecna nie dziala
                  * 
-                 * Po kliknieciu w przycisk zmieniamy tryb obracania/przesuwania na drugi dostepny.
-                 * 
-                 * jesli dodamy zgrabne menu manipulujace tymi boolami to tak mogloby to wygladac (sterowanie, nie animacja :) )
                  * 
                  * 
                  * 
@@ -75,96 +77,170 @@ public class TouchBehaviour : MonoBehaviour
 				
                 Plane targetPlane = new Plane(transform.up, transform.position);
 
-                foreach (Touch touch in Input.touches)
-                {
+                
 					
 					//operacje dla rotacji
 					
 					if(rotate) {
-
-						if(touch.phase == TouchPhase.Began && controls.HitTest(touch.position)){
-								move = true;
-								rotate = false;
-								return;
-						}						
-						
-	                    //Gets the ray at position where the screen is touched
-	                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-						
-	                    //Gets the position of ray along plane
-	                    float dist = 0.0f;
-	                    //Intersects ray with the plane. Sets dist to distance along the ray wher intersects
-	                    targetPlane.Raycast(ray, out dist);
+						foreach (Touch touch in Input.touches)
+                		{
+							if(touch.phase == TouchPhase.Began && controls.HitTest(touch.position)){
+									move = true;
+									rotate = false;
+									scale = false;
+									return;
+							}						
+							
+		                    //Gets the ray at position where the screen is touched
+		                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+							
+		                    //Gets the position of ray along plane
+		                    float dist = 0.0f;
+		                    //Intersects ray with the plane. Sets dist to distance along the ray wher intersects
+		                    targetPlane.Raycast(ray, out dist);
+		
+		                    //Returns point dist along the ray.
+		                    Vector3 planePoint = ray.GetPoint(dist);
+							
+							
+							//przeczytaj koordynaty,jesli palec dotknie ekranu
+		                    if (touch.phase == TouchPhase.Began)
+		                    {
+								StartX = touch.position.x;
 	
-	                    //Returns point dist along the ray.
-	                    Vector3 planePoint = ray.GetPoint(dist);
-						
-						
-						//przeczytaj koordynaty,jesli palec dotknie ekranu
-	                    if (touch.phase == TouchPhase.Began)
-	                    {
-							StartX = touch.position.x;
-
-	                    }
-	                    else if (touch.phase == TouchPhase.Moved)
-	                    {
-							
-							//odczytaj koncowe koordynaty
-							StopX = touch.position.x;
-							
-							float diff = StartX - StopX;
-							
-							//w zaleznosci od roznicy obrot w prawo albo lewo
-							if( diff > 0) {
-								ChairObj.transform.Rotate(Vector3.forward, 60 * Time.deltaTime);
+		                    }
+		                    else if (touch.phase == TouchPhase.Moved)
+		                    {
+								
+								//odczytaj koncowe koordynaty
+								StopX = touch.position.x;
+								
+								float diff = StartX - StopX;
+								
+								//w zaleznosci od roznicy obrot w prawo albo lewo
+								if( diff > 0) {
+									ChairObj.transform.Rotate(Vector3.forward, 60 * Time.deltaTime);
+								}
+								else {
+									ChairObj.transform.Rotate(-Vector3.forward, 60 * Time.deltaTime);	
+								}
+								
+								Debug.LogError("diff: "+diff.ToString()+"  STARTX:"+StartX.ToString()+"STOPX:"+StopX.ToString()+"__localscale:"+ChairObj.transform.localScale.ToString());
 							}
-							else {
-								ChairObj.transform.Rotate(-Vector3.forward, 60 * Time.deltaTime);	
-							}
-							
-							Debug.LogError("diff: "+diff.ToString()+"  STARTX:"+StartX.ToString()+"STOPX:"+StopX.ToString());
-							
+							return;
 	                    }
 
-						return;
+						
 					}
 					
-					if(move) {
-						if(touch.phase == TouchPhase.Began && controls.HitTest(touch.position)){
-								move = false;
-								rotate = true;
-								return;
-						}						
+					else if(move) {
+						foreach (Touch touch in Input.touches)
+                		{
+							if(touch.phase == TouchPhase.Began && controls.HitTest(touch.position)){
+									move = false;
+									scale = true;
+									rotate = false;									
+									return;
+							}						
+							
+		                    //Gets the ray at position where the screen is touched
+		                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+		                    //Gets the position of ray along plane
+		                    float dist = 0.0f;
+		                    //Intersects ray with the plane. Sets dist to distance along the ray wher intersects
+		                    targetPlane.Raycast(ray, out dist);
+		
+		                    //Returns point dist along the ray.
+		                    Vector3 planePoint = ray.GetPoint(dist);
+		
+		                    //True if finger touch began.
+		                    if (touch.phase == TouchPhase.Began)
+		                    {
+		                        // Ustaw ostatni punkt na obecny
+		                        lastPlanePoint = planePoint;
+		                    }
+		                    else if (touch.phase == TouchPhase.Moved)
+		                    {
+		                        // PrzesuĹ„ model i zmieĹ„ ostatni punkt
+		                        ChairObj.transform.position += planePoint - lastPlanePoint;
+		                        lastPlanePoint = planePoint;
+		                    }
+	
+							return;
+						}
+					}
+					
+					else if(scale) {
+					/*
+					 * Wczytuje 2 dotkniecia, obliczam odleglosc miedzy nimi,
+					 * po przesunieciu obliczam odleglosc koncowych punktow i odlegosc miedzy nimi.
+					 * Na tej podstawie skaluje obraz w gore albo w dol. 
+					 * jest ustawiona minimalna skala, zeby nie zniknal. Gornej nie ustawialem.
+					 * 
+					 * 
+					 * 
+					 * 
+					 * 
+					 */
+					
+						var tapCount = Input.touchCount;
+					
+					//przelaczenie do next trybu dopoki nie ma menu
+						if(Input.touches[0].phase == TouchPhase.Began && controls.HitTest(Input.touches[0].position)){
+							move = false;
+							scale = false;
+							rotate = true;									
+							return;
+						}
+					
+					//wczytanie 2 dotkniec w tym samym czasie
+						if(tapCount > 1) {
+						    var touch1 = Input.GetTouch(0);
+						    var touch2 = Input.GetTouch(1);
+							
+							if (touch1.phase == TouchPhase.Began && touch1.phase == TouchPhase.Began) {
+								//trzeba sie odwolac do globalnych, bo w next update wykona sie nizszy if
+								scaleStart1 = touch1.position;
+								scaleStart2 = touch2.position;
+								scaleStartDiff = Vector2.Distance(touch1.position,touch2.position);
+								Debug.LogError("Scale Diff:"+scaleStartDiff+"___ScaleObj: "+ChairObj.transform.localScale.ToString());
+							
+							}
+							else if (touch1.phase == TouchPhase.Moved && touch2.phase == TouchPhase.Moved) {
+								
+								Vector2 scaleEnd1 = touch1.position;
+								Vector2 scaleEnd2 = touch2.position;
+								float scaleEndDiff = Vector2.Distance(scaleEnd1,scaleEnd2);
+								
+								if(scaleEndDiff > scaleStartDiff) {
+									ChairObj.transform.localScale += new Vector3(0.0009F, 0.0009F, 0.0009F);
+								}
+								else {
+									if(ChairObj.transform.localScale.x < 0.1) 
+									{
+										ChairObj.transform.localScale = new Vector3(0.1F,0.1F,0.1F);
+									}
+									ChairObj.transform.localScale -= new Vector3(0.0009F, 0.0009F, 0.0009F);
+								}
+								
+							
+								//przepisanie nowych wartosci na poprzednie, zachowuje plynnosc
+								scaleStart1 = touch1.position;
+								scaleStart2 = touch2.position;
+								scaleStartDiff = Vector2.Distance(touch1.position,touch2.position);
+							
+							}
+							
+							
 						
-	                    //Gets the ray at position where the screen is touched
-	                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-	                    //Gets the position of ray along plane
-	                    float dist = 0.0f;
-	                    //Intersects ray with the plane. Sets dist to distance along the ray wher intersects
-	                    targetPlane.Raycast(ray, out dist);
-	
-	                    //Returns point dist along the ray.
-	                    Vector3 planePoint = ray.GetPoint(dist);
-	
-	                    //True if finger touch began.
-	                    if (touch.phase == TouchPhase.Began)
-	                    {
-	                        // Ustaw ostatni punkt na obecny
-	                        lastPlanePoint = planePoint;
-	                    }
-	                    else if (touch.phase == TouchPhase.Moved)
-	                    {
-	                        // PrzesuĹ„ model i zmieĹ„ ostatni punkt
-	                        ChairObj.transform.position += planePoint - lastPlanePoint;
-	                        lastPlanePoint = planePoint;
-	                    }
-
-						return;
+						}
+					
+						
 					}
                 }
             }
         }
-    }
+    
 
     private static TouchBehaviour m_Instance = null;
 
