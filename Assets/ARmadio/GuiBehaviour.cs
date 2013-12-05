f0f339511c58832f38617880d8ddd22aba1984ce
wesol05 <wesol05@gmail.com>
2013-12-05 08:05:18 +0100

    refactoring

--- Assets/ARmadio/GuiBehaviour.cs
+++ Assets/ARmadio/GuiBehaviour.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class GuiBehaviour2 : MonoBehaviour {
+public class GuiBehaviour : MonoBehaviour {
 	
 	private GameObject gui_actions;
 	private GameObject gui_modes;
@@ -15,6 +15,8 @@ public class GuiBehaviour2 : MonoBehaviour {
 	private GameObject gui_light;
 	private GameObject gui_activ;
 	private GameObject activeActive;
+	
+	Vector2 scrollPosition = new Vector2();
 	
 	void preInitialization() {
 		GameObject iT = GameObject.Find("ImageTarget");
@@ -73,6 +75,65 @@ public class GuiBehaviour2 : MonoBehaviour {
 		(gui_light.GetComponent(typeof(GUITexture)) as  GUITexture).texture = Resources.Load ("light_butt2") as Texture2D;
 		
 		preInitialization();
+	}
+	
+	void OnGUI() {
+		if(GlobalVariables.showInventory)
+		{
+			scrollPosition = GUILayout.BeginScrollView (
+				scrollPosition, GUILayout.Width (Screen.width), GUILayout.Height ((Screen.height * 0.15f)+20.0f));
+			GUILayout.BeginHorizontal();
+			
+			int i = 0;
+			foreach (var go in GlobalVariables.goList)
+			{
+				//TODO: sprawdzić, widocznie jakieś śmieci dodają się do goList
+				if(i<26){
+				if(GUILayout.Button((Resources.Load("InventoryThumbs/"+go.name) as Texture2D), GUILayout.Width (Screen.height * 0.15f), GUILayout.Height (Screen.height * 0.15f)))
+				{
+					if(GlobalVariables.add_)
+					{
+						GlobalVariables.showInventory = false;
+						GlobalVariables.add_ = false;
+						
+						GameObject go2 = (GameObject)GameObject.Instantiate(go);
+						go2.transform.localScale = go.transform.lossyScale/4;
+						go2.transform.localRotation = go.transform.rotation;
+						go2.transform.localPosition = go.transform.position;
+						go2.transform.parent = GameObject.Find("ImageTarget").transform;
+						
+						go2.SetActive(true);
+						
+						GlobalVariables.myList.Add(go2.gameObject);
+						GlobalVariables.numberOfModels++;
+						GlobalVariables.activeObject = go2.gameObject;
+					}
+					else if(GlobalVariables.change)
+					{
+						GlobalVariables.showInventory = false;
+						GlobalVariables.change = false;
+						
+						GameObject go2 = (GameObject)GameObject.Instantiate(go);
+						go2.transform.localScale = go.transform.lossyScale/4;
+						go2.transform.localRotation = go.transform.rotation;
+						go2.transform.localPosition = GlobalVariables.activeObject.transform.position;
+						go2.transform.parent = GameObject.Find("ImageTarget").transform;
+						
+						go2.SetActive(true);
+						
+						GlobalVariables.myList.Remove(GlobalVariables.activeObject);
+						DestroyImmediate(GlobalVariables.activeObject);
+						GlobalVariables.myList.Add(go2.gameObject);
+						GlobalVariables.activeObject = go2.gameObject;
+					}
+				}
+					
+				}i++;
+			}
+			
+			GUILayout.EndHorizontal();
+			GUILayout.EndScrollView ();
+		}
 	}
 	
 	// Update is called once per frame
